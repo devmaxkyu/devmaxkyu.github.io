2551257e2ca14f72fccfd936e1adcde93e1a8361
Zemin Wang <zeminwang@Zemins-iMac.local>
2019-12-14 00:00:05 +0800

    updates-2019-12-14

---
+++ precache-manifest.0e66d61b39b0e099ab0d6516f8b6b38d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62da6a9411762fa02a18872eb8f44eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f3dd580697388f96bb",
+    "url": "/static/css/main.16af1531.chunk.css"
+  },
+  {
+    "revision": "be5913393edeed1010f1",
+    "url": "/static/js/2.a2753ba7.chunk.js"
+  },
+  {
+    "revision": "78f3dd580697388f96bb",
+    "url": "/static/js/main.2aac5f20.chunk.js"
+  },
+  {
+    "revision": "cfa5135cb40f48bb4a23",
+    "url": "/static/js/runtime-main.082819b5.js"
+  }
+]);
